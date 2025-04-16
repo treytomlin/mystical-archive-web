fb01f80be24e1f6fe36508539b45941f34f4d866
Trey Tomlin <treytomlin91@gmail.com>
2025-04-16 05:21:17 -0400

    starting new components

---
+++ src/app/components/add-card/add-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-card.component.html',
+  styleUrl: './add-card.component.css'
+})
+export class AddCardComponent {
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button'; 
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
